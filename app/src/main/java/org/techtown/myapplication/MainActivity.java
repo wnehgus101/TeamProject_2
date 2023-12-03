77563ba21d301d8fec519c5770da2d6f904bab79
Ju Do Hyun <wnehgus88@mju.ac.kr>
2023-12-03 16:09:35 +0900

    회원가입 기능 예시

--- app/src/main/java/org/techtown/myapplication/MainActivity.java
+++ app/src/main/java/org/techtown/myapplication/MainActivity.java
@@ -2,7 +2,10 @@ package org.techtown.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button expertButton = findViewById(R.id.expert_button);
+        Button userButton = findViewById(R.id.user_button);
+
+        expertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateToSignUpPage(0);
+            }
+        });
+
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navigateToSignUpPage(1);
+            }
+        });
+    }
+
+    private void navigateToSignUpPage(int role){
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        intent.putExtra("ROLE", role);
+        startActivity(intent);
     }
 }
