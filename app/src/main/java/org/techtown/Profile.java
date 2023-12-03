6921ad43a14769236c553a2077abb4a92f4dbeeb
이해성 <108731391+Kangsaan0627@users.noreply.github.com>
2023-12-03 18:05:06 +0900

    Add files via upload

---
+++ app/src/main/java/org/techtown/Profile.java
@@ -0,0 +1,15 @@
+package com.example.teamproject2;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.intent.R;
+
+public class Profile extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile);
+    }
+}
