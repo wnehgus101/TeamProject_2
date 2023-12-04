00b8d160e4b4c9837150ceba0239a69525a2d197
Ju Do Hyun <wnehgus88@mju.ac.kr>
2023-12-04 16:17:22 +0900

    회원 가입 기능(test ui 포함) 구현, 회원 정보 데이터베이스 데이터 저장 구현(확인 완료)

--- app/src/main/java/org/techtown/myapplication/MainActivity.java
+++ app/src/main/java/org/techtown/myapplication/MainActivity.java
@@ -8,6 +8,12 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //전문가와 사용자의 버튼에 따라서 0,1의 값을 넘기고 해당 엑티비티를 여는 함수
+    private void startNextActivityWithValue(int user_types){
+        Intent intent = new Intent(MainActivity.this, SignUpTest.class);
+        intent.putExtra("user_type", user_types);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button imageButton = findViewById(R.id.profile);
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        imageButton.setOnClickListener(new View.OnClickListener() { //프로필 ui 전환 버튼
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Profile.class);
@@ -23,12 +29,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button imageButton2 = (Button) findViewById(R.id.review); //누르면 상담후기 화면 전환 버튼
+        Button imageButton2 = (Button) findViewById(R.id.review); //누르면 상담 후기 화면 전환 버튼
         imageButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Review.class);
                 startActivity(intent);
+            }
+        });
+
+        Button proSignUpTestButton = findViewById(R.id.signup_pro_test);
+        proSignUpTestButton.setOnClickListener(new View.OnClickListener() { //전문가 회원가입 테스트 전환 버튼
+            @Override
+            public void onClick(View view) {
+                startNextActivityWithValue(0);
+            }
+        });
+
+        Button userSignUpTestButton = findViewById(R.id.signup_user_test);
+        userSignUpTestButton.setOnClickListener(new View.OnClickListener() { //이용자 회원가입 테스트 전환 버튼
+            @Override
+            public void onClick(View view) {
+                startNextActivityWithValue(1);
             }
         });
     }
